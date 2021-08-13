1924e0a05144619211d704f2e50b5ddb57f93faa
Ozansk <57104812+Ozansk@users.noreply.github.com>
2021-08-13 22:26:58 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,18 +6,15 @@ const port = process.env.PORT || 5000;
 
 app.use(function(req, res, next) {
 
-    //to allow cross domain requests to send cookie information.
     res.header('Access-Control-Allow-Credentials', true);
     
-    // origin can not be '*' when crendentials are enabled. so need to set it to the request origin
     res.header('Access-Control-Allow-Origin',  req.headers.origin);
-    
-    // list of methods that are supported by the server
+
     res.header('Access-Control-Allow-Methods','OPTIONS,GET,PUT,POST,DELETE');
     
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, X-XSRF-TOKEN');
     
-        next();
+    next();
 });
 
 app.get('/api', (req, res) => {
